ed699a3d1fb9b29878a45a0f208ac8155a41c520
Scoooooooot <92255630+Scoooooooot@users.noreply.github.com>
2022-10-20 15:16:41 +0100

    file name change

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class dfgdh : MonoBehaviour
+public class Controller : MonoBehaviour
 {
     [SerializeField] float speed;
 
