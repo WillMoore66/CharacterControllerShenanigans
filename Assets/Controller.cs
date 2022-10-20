e996088fe56a9f0381ea79527d3ff2c4889d4ff7
Scoooooooot <92255630+Scoooooooot@users.noreply.github.com>
2022-10-20 16:07:14 +0100

    g

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -5,21 +5,32 @@ using UnityEngine.InputSystem;
 
 public class Controller : MonoBehaviour
 {
-    [SerializeField] float speed;
+    [SerializeField] float sprintSpeed;
+    [SerializeField] float walkSpeed;
+    private float speed;
+    private bool isSprinting;
+    private bool isGrounded;
 
     private void FixedUpdate() {
         var keyboard = Keyboard.current;
         if (keyboard == null)
             return;
+
+        speed = keyboard.eKey.isPressed ? sprintSpeed : walkSpeed;
+
         if (keyboard.aKey.isPressed) {
             Debug.Log("A was pressed");
-            this.transform.Translate(Vector3.right*speed);
+            this.transform.Translate(Vector3.right * speed);
         }
         if (keyboard.dKey.isPressed) {
             Debug.Log("d was pressed");
             this.transform.Translate(Vector3.left * speed);
         }
+       
+    }
 
+    private void GroundCheck() {
+       
     }
 
 }
