0d1059e3f9a1ad7473c5ba2bf8062f50a8832d35
Scoooooooot <92255630+Scoooooooot@users.noreply.github.com>
2022-10-20 15:10:41 +0100

    Initial commit

---
+++ Assets/dfgdh.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class dfgdh : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    private void FixedUpdate() {
+        var keyboard = Keyboard.current;
+        if (keyboard == null)
+            return;
+        if (keyboard.aKey.isPressed) {
+            Debug.Log("A was pressed");
+            this.transform.Translate(Vector3.right*speed);
+        }
+        if (keyboard.dKey.isPressed) {
+            Debug.Log("d was pressed");
+            this.transform.Translate(Vector3.left * speed);
+        }
+
+    }
+
+}
